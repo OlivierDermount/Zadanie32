405cbd40c63866cf48cad452d66254b34eafaa03
olivierdermount <olivierdermount@gmail.com>
2021-01-13 21:21:24 +0100

    Update

---
+++ Zadanie32/Gra.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zadanie32
+{
+    class Gra
+    {
+        static public int GeneratorLiczb()
+        {
+            Random random = new Random();
+            int wylosowanaLiczba = random.Next(0 , 100);
+            return wylosowanaLiczba;
+        }
+        
+        static public void weryfikacja(int wylosowanaLiczba)
+        {
+            var licznik = 1 ;
+            var wiadomosc = "";
+            uint wpisanaLiczba;
+
+            try
+            {
+                do
+                {
+                    Console.WriteLine(wiadomosc);
+                    wpisanaLiczba = uint.Parse(Console.ReadLine());
+
+                    wiadomosc = (wpisanaLiczba > wylosowanaLiczba) ?
+                        " Liczba jaką podałeś jest zaduża..." :
+                        " Liczba jaką podałeś jest zamała...";
+                    licznik++;
+
+                } while (wpisanaLiczba != wylosowanaLiczba);
+
+                Console.WriteLine($"Brawo! Zgadłeś po {licznik} próbach..... ");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+    }
+}
---
+++ Zadanie32/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Zadanie32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wylosowanaLiczba = Gra.GeneratorLiczb();
+            Console.WriteLine("Zgadnij jaką liczbę wylosował komputer?");
+            Gra.Weryfikacja(wylosowanaLiczba);
+        }
+    }
+}
