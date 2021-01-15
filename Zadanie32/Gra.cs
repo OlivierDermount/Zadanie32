5c3f239d571828a7fdc73c992a0786cb7b59c852
olivierdermount <olivierdermount@gmail.com>
2021-01-15 01:55:45 +0100

    first commit

--- Zadanie32/Gra.cs
+++ Zadanie32/Gra.cs
@@ -9,38 +9,31 @@ namespace Zadanie32
         static public int GeneratorLiczb()
         {
             Random random = new Random();
-            int wylosowanaLiczba = random.Next(0 , 100);
+            int wylosowanaLiczba = random.Next(0, 100);
             return wylosowanaLiczba;
         }
-        
+
         static public void weryfikacja(int wylosowanaLiczba)
         {
-            var licznik = 1 ;
+            var licznik = 1;
             var wiadomosc = "";
             uint wpisanaLiczba;
-
-            try
+            do
             {
-                do
-                {
-                    Console.WriteLine(wiadomosc);
-                    wpisanaLiczba = uint.Parse(Console.ReadLine());
+                Console.WriteLine(wiadomosc);
+                wpisanaLiczba = uint.Parse(Console.ReadLine());
 
-                    wiadomosc = (wpisanaLiczba > wylosowanaLiczba) ?
-                        " Liczba jaką podałeś jest zaduża..." :
-                        " Liczba jaką podałeś jest zamała...";
-                    licznik++;
+                wiadomosc = (wpisanaLiczba > wylosowanaLiczba) ?
+                    " Liczba jaką podałeś jest zaduża..." :
+                    " Liczba jaką podałeś jest zamała...";
+                licznik++;
 
-                } while (wpisanaLiczba != wylosowanaLiczba);
+            } while (wpisanaLiczba != wylosowanaLiczba);
 
-                Console.WriteLine($"Brawo! Zgadłeś po {licznik} próbach..... ");
-            }
-            catch(Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-
+            Console.WriteLine($"Brawo! Zgadłeś po {licznik} próbach..... ");
         }
 
+
     }
+
 }
--- Zadanie32/Program.cs
+++ Zadanie32/Program.cs
@@ -6,9 +6,22 @@ namespace Zadanie32
     {
         static void Main(string[] args)
         {
-            var wylosowanaLiczba = Gra.GeneratorLiczb();
-            Console.WriteLine("Zgadnij jaką liczbę wylosował komputer?");
-            Gra.Weryfikacja(wylosowanaLiczba);
+            while (true)
+            {
+                try
+                {
+                    var wylosowanaLiczba = Gra.GeneratorLiczb();
+                    Console.WriteLine("Zgadnij jaką liczbę wylosował komputer?");
+                    Gra.weryfikacja(wylosowanaLiczba);
+                    Console.WriteLine("Gramy dalej? t/n");
+                    var takNie = Console.ReadLine();
+                    if (takNie == "n") break;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
         }
     }
 }
